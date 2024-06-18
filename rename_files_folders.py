899e87d1bc8f03b077653c51a8e70e32c9362a4f
Marcos Garcia Vega <marcosgarciavega@gmail.com>
2024-06-18 06:33:46 -0500

    create structure of rename_files command

--- rename_files_folders.py
+++ rename_files_folders.py
@@ -6,8 +6,39 @@ class RenameApp():
         super().__init__()
         # self.grid()
 
-        self.entrythingy = tk.Label(text="Welcome to the File/Folder Rename Application!",font= (0,17))
-        self.entrythingy.grid(column=0, row=0)
+        self.introtext = tk.Label(text="Welcome to the File/Folder Rename Application!",font= (0,17))
+        self.introtext.grid(column=0, row=0, columnspan=2)
+
+        self.subtext = tk.Label(text="Please enter the absoluite path of where you want to rename your files and folders")
+        self.subtext.grid(column= 0, row=1, columnspan=2)
+
+        self.exampletext = tk.Label(text ="An example -  C:\\Users\\gama6p9\\Downloads")
+        self.exampletext.grid(column = 0, row=2, columnspan=2)
+
+        self.exampletext = tk.Label(text ="File Path Here:")
+        self.exampletext.grid(column = 0, row=3)
+        
+        self.enterpath = tk.Entry(width=50)
+        self.enterpath.grid(column = 1, row = 3)
+
+        self.begstring = tk.Label(text ="What text do you want to rename?")
+        self.begstring.grid(column = 0, row=4)
+        
+        self.begstring_input = tk.Entry(width=50)
+        self.begstring_input.grid(column = 1, row = 4)        
+
+        self.endstring = tk.Label(text ="What do you want to replace it with?")
+        self.endstring.grid(column = 0, row=5)
+        
+        self.endstring_input = tk.Entry(width=50)
+        self.endstring_input.grid(column = 1, row = 5)    
+
+
+        self.execute_button = tk.Button(text="Execute")
+        self.execute_button.grid(column=0, row=6,columnspan=2)       
+
+        self.return_button = tk.Button(text="Return to main menu")
+        self.return_button.grid(column=0,row=7, columnspan=2) 
 
         # Create the application variable.
         # self.contents = tk.StringVar()
